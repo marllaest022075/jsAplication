09ae48c8869a7e55d71d5b273e0075588a67feff
Martin Estrada <marllaest2075@hotmail.com>
2022-07-27 06:26:46 -0700

    Rutas Listas ya se puede acceder a las paginas con un archivo de rutas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import  express  from "express";
+import ejs from "ejs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import indexRouter from "./routes/index";
 
 const port = process.env.PORT || 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(indexRouter);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
