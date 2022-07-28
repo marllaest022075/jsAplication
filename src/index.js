61ebc7e9872a17f5706e3b545b2a3abde532fbaf
Martin Estrada <marllaest2075@hotmail.com>
2022-07-27 19:31:55 -0700

    Pasar valores desde js a ejs(html)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import  express  from "express";
 import ejs from "ejs";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
-import indexRouter from "./routes/index";
+import indexRouter from './routes/index.js';
 
 const port = process.env.PORT || 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -16,3 +16,4 @@ app.use(indexRouter);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+///C:\Users\Marty\Desktop\jsAplication\src\routes
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,13 @@ import { Router } from 'express';
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { title: "Home" });
 });
 router.get("/about", (req, res) => {
-    res.render("about");
+    res.render("about", { title: "About" });
 });
 router.get("/contact", (req, res) => {
-    res.render("contact");
+    res.render("contact", { title: "Contact" });
 });
 
 export default router;
